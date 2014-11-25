5db489eb9c43e0b522515175ec76c24b8bcfc509
ChienliMa <maqianlie@gmail.com>
2014-11-25 20:42:42 +0800

    Some change of namespace; Now it can crawl exo center info

--- Rakefile
+++ Rakefile
@@ -1,60 +1,28 @@
 # -*- coding:utf-8 -*-
 require 'active_record'
-require '../lib/zhanhui'
-require '../lib/operation'
+require 'yaml'
+require './lib/zhanhui'
 
-namespace zhanhui do
+namespace :operation do
 
-  config = YAML::load( File.open('localhost.yml') )
+  config = YAML::load( File.open('remote.yaml') )
   ActiveRecord::Base.establish_connection( config )
 
   class Exhibition < ActiveRecord::Base 
   end
   class CityCode < ActiveRecord::Base
   end
-
-  desc "Update exhibition list"
-  task :update_list do
-    Zhanhui.get_exi_list().each do |exhibition|
-      begin
-        Exhibition.new(exhibition).save
-      rescue =>e
-        p "Fail to add:" + exhibition.to_s
-      end
-    end
+  class BaseGeneralExpoCenter < ActiveRecord::Base
   end
 
-  desc "Query and update city code from another table"
-  task :update_city_code do
-    Exhibition.where(city_code:nil).each do |exhibition|
-      begin
-        exhibition[:city_code] = CityCode.where( city:exhibition[:city]).first[:code]
-        exhibition.save
-      rescue
-        p "error updating city_code" + exhibition[:name]
-      end
-    end
-  end
+  desc "Crawl Expo Info"
+  task :crawl_expo_info do
+    # get expo list
+    exhibitions = Zhanhui.get_expos()
 
-  desc "Clean updated( early than today ) exhibitions"
-  task :wash_list()
-    """
-    Delete outdated record
-    """
-    Exhibition.all.each do |exhibition|
-      if exhibition[:date] < Time.now 
-        p "Delete outdated exhibition:" + exhibition[:name]
-        exhibition.destory!
-      end
-    end
-  end
-
-  desc "update details. default thread_num is 3"
-  task :update_details do
-    thread_num = 3
-
-    works = []
-    Exhibition.where(has_detail:nil).each{ |record| works << record }
+    # fetch each info of each expo
+    thread_num = 5
+    works = exhibitions
 
     workers = (0..thread_num).map do 
       # start a new thread
@@ -66,13 +34,10 @@ namespace zhanhui do
             sleep( 1.second )
             begin
               p "update details of:" + work[:name].force_encoding('utf-8')
-              work = Zhanhui.get_details(work) 
-              work[:has_detail] = true      
+              info = Zhanhui.get_expo_info(work) 
+              Exhibition.new( info ).save
             rescue => e
               puts "error loading page:" + work[:page]
-              work[:has_detail] = false
-            ensure
-              work.save    
             end
           end
         rescue ThreadError
@@ -83,5 +48,38 @@ namespace zhanhui do
     workers.map(&:join); 
   end
 
+
+  desc "Crawl Expo Center Info"
+  task :crawl_expo_center_info do
+    # get expo center id list
+    center_ids = Zhanhui.get_expo_center_ids()
+    
+    # fetch info of each expo center
+    thread_num = 8
+    works = ids
+
+    workers = (0..thread_num).map do 
+      # start a new thread
+      Thread.new do
+        begin 
+          while work = works.pop()
+            raise "Works finished" if work == nil
+            # sleep for 1 second
+            sleep( 1.second )
+            begin
+              p "update details of:" + work[:name].force_encoding('utf-8')
+              info = Zhanhui.get_expo_center_info(work) 
+              BaseGeneralExpoCenter.new( info ).save
+            rescue => e
+              puts "error loading page:" + work[:page]
+            end
+          end
+        rescue ThreadError
+        end
+      end 
+    end # works.map
+    # hold main thread 
+    workers.map(&:join); 
+  end
 
 end
--- lib/zhanhui.rb
+++ lib/zhanhui.rb
@@ -10,19 +10,19 @@ class Zhanhui
   2. get details
     list.each{ |x| Zhanhui.get_details( x ) } 
   """
-     
+
+  private
   def self.request( url )
     """
     Mimic broswer behavior to sent HTTP request
     to avoid blocking
     """
     # parse URL get URI
-    uri = url.match(/\/exh\D.*?html/).to_s
+    uri = url.match(/com.*?\z/).to_s[3..-1]
     http_request = "GET #{uri} HTTP/1.1\r\n"+
     "Host:www.haozhanhui.com\r\n"+
     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"+
     "Connection:keep-alive\r\n"+
-    "Referer: http://www.haozhanhui.com/zhanlanjihua/\r\n"+
     "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0\r\n\r\n"
     socket = TCPSocket.open( 'www.haozhanhui.com', 80)
 
@@ -38,7 +38,11 @@ class Zhanhui
     return html
   end
 
-  def self.get_details( param )
+  ##################################
+  #  Methods below parse Expo Info #
+  ################################## 
+
+  def self.get_expo_info( param )
     """
     Enter exhibition info page using :page in param
     THen parse details of exhibitons
@@ -72,7 +76,7 @@ class Zhanhui
     return param
   end
 
-  def self.get_exi_list()
+  def self.get_expos()
     url = "http://www.haozhanhui.com/zhanlanjihua/"
     doc = Nokogiri::HTML(open(url))
 
@@ -87,19 +91,88 @@ class Zhanhui
       exhibition[:date] = text[0,10]
       exhibition[:name] = text.match(/】\S\S([0-9]+|[a-zA-Z]+).+/).to_s[3..-1]
 
-      text.gsub(/【.*?】/).each do |x|
-        if exhibition[:cata] == ''
-          exhibition[:cata] = x[1,x.length-2] 
-          continue
-        end
-        exhibition[:city] = x[1,x.length-2] 
-      end
+      # Deprecatem, we don't need city info now
+      # text.gsub(/【.*?】/).each do |x|
+      #   if exhibition[:cata] == ''
+      #     exhibition[:cata] = x[1,x.length-2] 
+      #     continue
+      #   end
+      #   exhibition[:city] = x[1,x.length-2] 
+      # end
       # get info page
       exhibition[:page] = li.search("a").attribute("href").value
 
       exhibitions << exhibition
     end
     return exhibitions
+  end
+
+
+  #########################################
+  # Methods below crawl Expo centers info #
+  #########################################
+  def get_expo_center_ids()
+
+    return ids
+  end
+
+  def get_expo_center_info( id )
+    center = {}
+    center[:id] = id
+    url = "http://www.haozhanhui.com/place/place_detail_#{id}.html"
+    # get name
+
+    # get ciry, location, website, intro and contact
+    url = "http://www.haozhanhui.com/place/place_detail_#{id}.html"
+    page = Nokogiri:HTML( request( url ) )
+    page.search("div[@class='box exh_box']").each do |x|
+      if x.search("h3").text[-4..-1] == "常用信息"
+        # get name
+        center[:name] = x.search("h3").text[0..-5]
+        # get common info
+        x.search("ul").search("li").each do |li|
+          li = li.text
+          if li[0..3] =="展馆城市"
+            center[:city] = li[5..-1]
+          elsif li[0..3] =="展馆位置"
+            center[:location] = li[5..-1]
+          elsif li[0..3] =="展馆网址"
+            center[:offcial_website] = li[5..-1]
+          end
+        end
+      elsif x.search("h3").text[-4..-1] == "展馆简介"
+        center[:intro] = x.search("div[@class='box-bd placedetail']").text.strip
+      elsif x.search("h3").text[-4..-1] == "联系信息"
+        center[:contact] = x.search("div[@class='box-bd placedetail']").text.strip
+      end
+    end
+
+    # get nearby information
+    # get bus information
+    center[:bus] = get_expo_center_extra_info( '公交车站', id )
+
+    # get tracffic information
+    center[:traffic] = get_expo_center_extra_info( '公交车站', id )
+
+    # get bank information
+    center[:bank] = get_expo_center_extra_info( '公交车站', id )
+
+    # get extra infromation
+    text = ''
+    text += get_expo_center_extra_info( '购物', id )
+    text += get_expo_center_extra_info( '餐厅', id )
+    text += get_expo_center_extra_info( '娱乐休闲_', id )
+    center[:extra] = text
+
+    return center
+  end
+
+  private
+  def self.get_expo_center_extra_info( tpye, id )
+    url = 'http://www.haozhanhui.com/place/place_service_#{type}_#{id}.html'
+    page = Nokogiri::HTML( request( url ))
+    page.search("div[@class='box exh_box']")\
+          .search("div[@class='box-bd placedetail']").text.strip
   end
 
 end
